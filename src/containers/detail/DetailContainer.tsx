701c626e6645ceac5d18ab88c5cac4047c4bfb2c
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2024-12-06 13:32:55 +0400

    detail page improvement

--- src/containers/detail/DetailContainer.tsx
+++ src/containers/detail/DetailContainer.tsx
@@ -3,6 +3,12 @@ import AnimatedSection from "../../components/ui/AnimatedSection.tsx";
 import {useParams} from "react-router-dom";
 import useGetOneArtwork from "../../hooks/service/useGetOneArtwork.tsx";
 import {loading_2} from "../../utils/assets.tsx";
+import {IoShareSocialSharp} from "react-icons/io5";
+import {MdLocationOn} from "react-icons/md";
+import {MdDownload} from "react-icons/md";
+import {FaRegCalendarAlt} from "react-icons/fa";
+import {LuPalette} from "react-icons/lu";
+import {Tooltip} from "react-tooltip";
 
 const DetailContainer: FC = (): JSX.Element => {
 
@@ -17,13 +23,52 @@ const DetailContainer: FC = (): JSX.Element => {
 
     if (isError) return <div>Something went wrong</div>
 
-    console.log(data);
-
     return <AnimatedSection class_name={"detail_container"}>
         <h1 className={"art_title"}>{data?.data.title}</h1>
 
         <div className={"detail_box"}>
-            <div className={"detail_box_left_side"}>a</div>
+            <div className={"detail_box_left_side"}>
+
+                <div className={"icons_area"}>
+
+                    <div data-tooltip-id={"share"} data-tooltip-content={"Share"} className={"detail_icon_wrapper"}>
+                        <IoShareSocialSharp className={"icon"}/>
+                    </div>
+
+                    <div data-tooltip-id={"download"} data-tooltip-content={"Download image"}
+                         className={"detail_icon_wrapper"}>
+                        <MdDownload className={"icon download"}/>
+                    </div>
+
+
+                    <Tooltip id={"share"}/>
+                    <Tooltip id={"download"}/>
+
+                </div>
+
+                <hr/>
+
+                <div className={"detail_box_left_side_info"}>
+
+                    <div className={"detail_box_left_side_info_item"}>
+                        <LuPalette className={"info_item_icon"}/> <h1>{data?.data.artist_title}</h1>
+                    </div>
+
+                    <div className={"detail_box_left_side_info_item"}>
+                        <MdLocationOn className={"info_item_icon"}/> <h1>{data?.data.place_of_origin}</h1>
+                    </div>
+
+                    <div className={"detail_box_left_side_info_item"}>
+                        <FaRegCalendarAlt className={"info_item_icon"}/> <h1>{data?.data.artist_display}</h1>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
             <div className={"detail_box_main_side"}>
                 <figure>
                     <img src={`https://www.artic.edu/iiif/2/${data?.data.image_id}/full/843,/0/default.jpg`}
@@ -45,7 +90,14 @@ const DetailContainer: FC = (): JSX.Element => {
 
 
             </div>
-            <div className={"detail_box_right_side"}>c</div>
+            <div className={"detail_box_right_side"}>
+
+                {data?.data.short_description && <>
+                    <p className={"short_description"}>{data?.data.short_description}</p>
+                    <hr/>
+                </>}
+                <p className={"license_text"}>{data?.info.license_text}</p>
+            </div>
         </div>
 
 
