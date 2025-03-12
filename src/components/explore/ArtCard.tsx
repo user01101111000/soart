23c2495b44183588f6586724069c62935f7bc95d
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2025-03-12 14:01:27 +0400

    image bug update

--- src/components/explore/ArtCard.tsx
+++ src/components/explore/ArtCard.tsx
@@ -1,6 +1,6 @@
-import {FC, JSX, SyntheticEvent} from "react";
-import {NavigateFunction, useNavigate} from "react-router-dom";
-import {ArtCardProps} from "../../types/component/component_types.ts";
+import { FC, JSX, SyntheticEvent } from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import { ArtCardProps } from "../../types/component/component_types.ts";
 
 const ArtCard: FC<ArtCardProps> = (props: ArtCardProps): JSX.Element => {
 
@@ -13,13 +13,13 @@ const ArtCard: FC<ArtCardProps> = (props: ArtCardProps): JSX.Element => {
         <figure>
             <img
 
-                src={props.artwork.thumbnail.lqip}
-                data-src={`https://www.artic.edu/iiif/2/${props?.artwork?.image_id}/full/843,/0/default.jpg`}
+                src={props?.artwork?.thumbnail?.lqip ?? "https://img.freepik.com/free-vector/white-blurred-background_1034-249.jpg"}
+                data-src={props?.artwork?.image_id ? `https://www.artic.edu/iiif/2/${props?.artwork?.image_id}/full/843,/0/default.jpg` : "https://img.freepik.com/free-vector/white-blurred-background_1034-249.jpg"}
                 alt={props?.artwork?.thumbnail?.alt_text}
                 onLoad={(e: SyntheticEvent<HTMLImageElement>): void => {
                     e.currentTarget.src = e.currentTarget.dataset.src || e.currentTarget.src;
                 }}
-                loading={"lazy"}/>
+                loading={"lazy"} />
         </figure>
         <h1>{props?.artwork?.title}</h1>
         <p>{props?.artwork?.artist_display}</p>
